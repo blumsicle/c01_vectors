ae765b1b60c5c00812f1c0fcac91a9d62f4732f7
Brian Blumberg <blumsicle@gmail.com>
2022-07-22 17:45:07 -0700

    Example 01-03

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,11 @@ fn main() {
     nannou::app(model).update(update).run();
 }
 
-struct Model {
-    location: Vec2,
-    velocity: Vec2,
-}
+struct Model {}
 
 impl Default for Model {
     fn default() -> Self {
-        Self {
-            location: vec2(100.0, 100.0),
-            velocity: vec2(2.5, 5.0),
-        }
+        Self {}
     }
 }
 
@@ -32,29 +26,24 @@ fn model(app: &App) -> Model {
     }
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {
-    model.location += model.velocity;
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-    let bounds = app.window_rect();
-
-    if model.location.x <= bounds.left() || model.location.x >= bounds.right() {
-        model.velocity.x *= -1.0;
-    }
-
-    if model.location.y <= bounds.bottom() || model.location.y >= bounds.top() {
-        model.velocity.y *= -1.0;
-    }
-}
-
-fn view(app: &App, model: &Model, frame: Frame) {
+fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
+
+    let mouse = vec2(app.mouse.x, app.mouse.y);
 
     draw.background().color(rgb(0.2, 0.4, 0.6));
 
+    draw.line()
+        .color(rgb(0.2, 0.6, 0.4))
+        .points(Vec2::ZERO, mouse)
+        .weight(2.0);
+
     draw.ellipse()
-        .color(rgb8(175, 175, 175))
-        .radius(16.0)
-        .xy(model.location);
+        .color(rgb(0.4, 0.8, 0.6))
+        .radius(8.0)
+        .xy(mouse);
 
     draw.to_frame(app, &frame).unwrap();
 }
