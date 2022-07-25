68a73781c7f83d34c6a47d8da6d6706b20d333b5
Brian Blumberg <blumsicle@gmail.com>
2022-07-24 18:01:36 -0700

    Example 01-07

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-use std::ops::Mul;
+use std::ops::{Mul, Sub};
 
 use nannou::prelude::*;
 
-fn main() {
-    nannou::app(model).update(update).run();
+struct Mover {
+    location: Vec2,
+    velocity: Vec2,
+    color: Hsv,
 }
 
-struct Model {}
-
-impl Default for Model {
-    fn default() -> Self {
-        Self {}
+impl Mover {
+    fn update(&mut self) {
+        self.location += self.velocity;
     }
+
+    fn draw(&self, draw: &Draw) {
+        draw.ellipse()
+            .color(self.color)
+            .radius(8.0)
+            .xy(self.location);
+    }
+
+    fn check_edges(&mut self, bounds: &Rect) {
+        if self.location.x < bounds.left() {
+            self.location.x = bounds.right();
+        } else if self.location.x > bounds.right() {
+            self.location.x = bounds.left();
+        }
+
+        if self.location.y < bounds.bottom() {
+            self.location.y = bounds.top();
+        } else if self.location.y > bounds.top() {
+            self.location.y = bounds.bottom();
+        }
+    }
+}
+
+struct Model {
+    movers: Vec<Mover>,
 }
 
 fn model(app: &App) -> Model {
@@ -23,30 +48,45 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    Model {
-        ..Default::default()
+    let bounds = app.window_rect();
+    let mut movers = Vec::<Mover>::new();
+
+    for _ in 0..10 {
+        let location = random::<Vec2>().sub(0.5).mul(bounds.wh());
+        let velocity = random::<Vec2>().sub(0.5).mul(10.0);
+        let color = hsv(random_f32(), 0.5, 0.7);
+
+        movers.push(Mover {
+            location,
+            velocity,
+            color,
+        })
+    }
+
+    Model { movers }
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let bounds = app.window_rect();
+
+    for mover in &mut model.movers {
+        mover.update();
+        mover.check_edges(&bounds);
     }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-
-    let mut mouse = vec2(app.mouse.x, app.mouse.y);
-    mouse = mouse.normalize_or_zero().mul(100.0);
 
     draw.background().color(rgb(0.2, 0.4, 0.6));
 
-    draw.line()
-        .color(rgb(0.2, 0.6, 0.4))
-        .points(Vec2::ZERO, mouse)
-        .weight(2.0);
-
-    draw.ellipse()
-        .color(rgb(0.4, 0.8, 0.6))
-        .radius(8.0)
-        .xy(mouse);
+    for mover in &model.movers {
+        mover.draw(&draw);
+    }
 
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn main() {
+    nannou::app(model).update(update).run();
 }
